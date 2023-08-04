fdd8b237c5f2a67cae215b18c4e26b2ef04f3a39
Nicole Peralta <89421724+nicolescania@users.noreply.github.com>
2023-08-04 13:05:00 -0400

    updated books router

--- Back-End-API/Server/Routes/books.js
+++ Back-End-API/Server/Routes/books.js
@@ -8,12 +8,12 @@ const book_1 = require("../controllers/book");
 const router = express_1.default.Router();
 const bookController = require('../controllers/book');
 router.get('/api/books', bookController.getAllBooks);
-router.post('/api/createbooks', book_1.upload, bookController.createBook);
-router.get('/api/getbook/:id', bookController.getBookById, (req, res) => {
+router.post('/api/books/create', book_1.upload, bookController.createBook);
+router.get('/api/book/:id', bookController.getBookById, (req, res) => {
     res.json(res.book);
 });
-router.patch('/api/updatebook/:id', bookController.getBookById, bookController.updateBook);
-router.delete('/api/deletebook/:id', bookController.getBookById, bookController.deleteBook);
+router.patch('/api/book/update/:id', bookController.getBookById, bookController.updateBook);
+router.delete('/api/book/delete/:id', bookController.getBookById, bookController.deleteBook);
 module.exports = router;
 exports.default = router;
 //# sourceMappingURL=books.js.map
--- Back-End-API/Server/Routes/books.ts
+++ Back-End-API/Server/Routes/books.ts
@@ -7,26 +7,24 @@ const router = express.Router();
 
 const bookController = require('../controllers/book')
 
-
-
 // GET BOOK LIST
 router.get('/api/books', bookController.getAllBooks)
 
 //CREATE A NEW BOOK
-router.post('/api/createbooks', upload, bookController.createBook)
+router.post('/api/books/create', upload, bookController.createBook)
 
 
 //MILDWARE 
-router.get('/api/getbook/:id', bookController.getBookById, (req: any, res: any) => {
+router.get('/api/book/:id', bookController.getBookById, (req: any, res: any) => {
     res.json(res.book)
 
 })
 
 //UPDATE BOOK 
-router.patch('/api/updatebook/:id', bookController.getBookById, bookController.updateBook)
+router.patch('/api/book/update/:id', bookController.getBookById, bookController.updateBook)
 
 //DELETE BOOK 
-router.delete('/api/deletebook/:id', bookController.getBookById, bookController.deleteBook)
+router.delete('/api/book/delete/:id', bookController.getBookById, bookController.deleteBook)
 
 
 
